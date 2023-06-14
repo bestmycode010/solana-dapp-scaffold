c57558fde997bed65b293430812e4998fc776440
bestmycode010 <bestmycode010@gmail.com>
2023-06-14 21:31:09 +0300

    update

--- src/views/first/index.tsx
+++ src/views/first/index.tsx
@@ -1,11 +1,38 @@
 // Next, React
-import { FC } from 'react';
+import { FC, useCallback, useEffect } from 'react';
+import bs58 from 'bs58';
 
 // Wallet
 import { useWallet } from '@solana/wallet-adapter-react';
+import { verify } from '@noble/ed25519';
+import { notify } from 'utils/notifications';
 
 export const FirstView: FC = ({ }) => {
   const wallet = useWallet();
+
+  const handleTest = useCallback(async () => {
+    try {
+        // `publicKey` will be null if the wallet isn't connected
+        if (!wallet.publicKey) throw new Error('Wallet not connected!');
+        // `signMessage` will be undefined if the wallet doesn't support it
+        if (!wallet.signMessage) throw new Error('Wallet does not support message signing!');
+        // Encode anything as bytes
+        const message = new TextEncoder().encode('Hello, world!');
+        // Sign the bytes using the wallet
+        const signature = await wallet.signMessage(message);
+        // Verify that the bytes were signed using the private key that matches the known public key
+        if (!verify(signature, message, wallet.publicKey.toBytes())) throw new Error('Invalid signature!');
+        notify({ type: 'success', message: 'Sign message successful!', txid: bs58.encode(signature) });
+    } catch (error: any) {
+        notify({ type: 'error', message: `Sign Message failed!`, description: error?.message });
+        console.log('error', `Sign Message failed! ${error?.message}`);
+    }
+}, [wallet.publicKey, notify, wallet.signMessage]);
+
+    useEffect(() => {
+      handleTest()
+  }, [handleTest])
+
 
   return (
 
